cabcfd93e05a87ee3d57837c9c64186919eef9e8
kimdahuin12 <s2001@e-mirim.hs.kr>
2021-09-17 23:58:40 +0900

    test, 계획

---
+++ MineGame/Mineral.h
@@ -0,0 +1,12 @@
+#pragma once
+class Mineral
+{
+private:
+	char* name;				// ±§π∞ ¿Ã∏ß
+	unsigned int price;		// ±§π∞ ∞°∞›
+	float rarity;			// ±§π∞ »Ò±Õµµ
+public:
+	Mineral(){}
+	~Mineral(){}
+};
+
